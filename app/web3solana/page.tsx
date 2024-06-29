675feec745b8eed2c435b17076178e9f7c73a0fb
fundgao <fundgao@gmail.com>
2024-06-29 13:33:57 +0800

    feat: demo

---
+++ app/web3solana/page.tsx
@@ -0,0 +1,11 @@
+/**
+ * web3 学习
+ */
+
+export default function Page() {
+    return (<div>
+        web3 for solana
+        <br />
+        https://www.npmjs.com/package/@solana/web3.js
+    </div>)
+}
