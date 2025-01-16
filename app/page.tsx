cce325328a47444200a5f2780ba56e3806fa81f5
Fund <fundgao@gmail.com>
2025-01-16 20:51:07 +0800

    feat: add story

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
-import Marquee from "@/components/ui/marquee";
+import { LineShadowText } from "@/components/ui/line-shadow-text";
 import { VelocityScroll } from "@/components/ui/scroll-text";
+// import FlipClock from "@/components/FlipClock/FlipClock";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -35,23 +37,48 @@ export default function Home() {
             &nbsp;&nbsp;Goldoge 金狗科技 助您暴富&nbsp;&nbsp;🚀
           </VelocityScroll>
         </div>
-        <div className="content-top mt-40 flex justify-center">
-          <a
-            className="text-white text-2xl font-bold"
-            href="https://goldog.netlify.app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="inline-block"
-              width={100}
-              height={24}
-              priority
-            />
-            Goldoge 金狗科技
-          </a>
+        <div className="content-top mt-28 flex justify-center flex-col items-center space-y-4">
+          {/* <FlipClock /> */}
+          <div className="time text-3xl font-semibold text-white font-sans">
+            20:32
+          </div>
+          <h1 className="font-semibold leading-none tracking-tighter sm:text-3xl md:text-6xl text-white">
+            Fund
+            <LineShadowText className="italic" shadowColor="white">
+              OS
+            </LineShadowText>
+          </h1>
+        </div>
+        <div className="content-bottom absolute w-full bottom-[100px]">
+          <div className="icons flex justify-center space-x-4">
+            <Link href="https://goldog.netlify.app/" target="_blank">
+              <Image
+                src="/image/ai16z2.avif"
+                alt="ai16z Logo"
+                className="rounded-3xl"
+                width={80}
+                height={80}
+                priority
+              />
+              <p className="text-center text-white mt-1">Goldoge</p>
+            </Link>
+          </div>
+          <div className="texts flex justify-center space-x-4 text-white mt-9">
+            <Link
+              href="https://github.com/fundgao"
+              target="_blank"
+              className="px-4 py-[4px] bg-slate-600/30 rounded-3xl"
+            >
+              Github
+            </Link>
+            <Link
+              href="mailto:fundgao@163.com"
+              target="_blank"
+              className="px-4 py-[4px] bg-slate-600/30 rounded-3xl"
+            >
+              Email
+            </Link>
+          </div>
         </div>
       </div>
     </main>
