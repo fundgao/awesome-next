1323c47d5ecfa25a3d037ea8f68b9c3f83becdd7
Fund <fundgao@gmail.com>
2025-03-18 09:28:25 +0800

    feat: shadcn admin启动

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 
 const nextConfig = {
   experimental: {
+    // DEMO https://github.com/vercel/next-view-transition-example/blob/main/app/card/page.tsx
+    viewTransition: true,
     useCache: true,
     turbo: {
       // Example: adding an alias and custom file extension
