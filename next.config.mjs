b4be6d458ca73dd81b8de27f00401ae35205e755
Fund <fundgao@gmail.com>
2025-03-18 09:09:02 +0800

    feat: shadcn admin启动

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  experimental: {
+    useCache: true,
+    turbo: {
+      // Example: adding an alias and custom file extension
+      resolveAlias: {
+        underscore: "lodash",
+      },
+      resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+  },
+};
 
 export default nextConfig;
-
