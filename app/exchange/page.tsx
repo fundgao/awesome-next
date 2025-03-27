33d41d5583fe332077484aecf799d2d6cbbc23e7
Fund <fundgao@gmail.com>
2025-03-27 18:49:30 +0800

    feat: shadcn admin启动

--- app/exchange/page.tsx
+++ app/exchange/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <div className="wrapper-headline">Mexc</div>
         <div className="wrapper-markets">自选币列表</div>
         <div className="wrapper-kline">图表</div>
-        <div className="wrapper-orderbook-deals">委托订单</div>
+        <div className="wrapper-orderbook">委托订单</div>
         <div className="wrapper-trade">现货交易</div>
         <div className="wrapper-orders">当前委托</div>
         <div className="wrapper-balance">资产</div>
