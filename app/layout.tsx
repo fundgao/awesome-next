b2cf142e1d2357e2e784317ef840c4a44db965c6
Fund <fundgao@gmail.com>
2025-03-30 12:20:58 +0800

    feat: shadcn admin启动

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,33 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FundOS - The Operating System for Fund",
   description: "FundOS is an open-source protocol for autonomous Fund.",
+  keywords: "Crypto | AIGC | Stock | X",
+  openGraph: {
+    type: "website",
+    url: "https://av16z.vercel.app/",
+    title: "FundOS - The Operating System for Fund",
+    description: "FundOS is an open-source protocol for autonomous Fund.",
+    siteName: "FundOS",
+    images: [
+      {
+        url: "https://example.com/og.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://av16z.vercel.app/",
+    creator: "@FundGao",
+    images: "https://example.com/og.png",
+  },
 };
 
 export default function RootLayout({
@@ -16,7 +37,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className + " relative"}>{children}</body>
+      <body className={inter.className + " relative"}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
