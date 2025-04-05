635b0476b8484f50ab3f4bacebdbbaefe9901267
Fund <fundgao@gmail.com>
2025-04-05 15:14:09 +0800

    feat: shadcn admin启动

--- app/exchange/page.tsx
+++ app/exchange/page.tsx
@@ -11,7 +11,16 @@ export default function Page() {
         <div className="wrapper-kline">图表</div>
         <div className="wrapper-orderbook">委托订单</div>
         <div className="wrapper-trade">现货交易</div>
-        <div className="wrapper-orders">当前委托</div>
+        <div className="wrapper-orders">
+          <div className="orders-header">
+            <div className="left">
+              <p className="orders-header-title">当前委托(0)</p>
+              <p className="orders-header-title">历史委托</p>
+              <p className="orders-header-title">历史成交</p>
+              <p className="orders-header-title">当前持仓(0)</p>
+            </div>
+          </div>
+        </div>
         <div className="wrapper-balance">资产</div>
       </div>
     </>
