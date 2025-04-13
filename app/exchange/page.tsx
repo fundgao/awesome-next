76cc8f48d0db2e4a097c41482c08310d02b36d6e
Fund <fundgao@gmail.com>
2025-04-13 20:38:29 +0800

    feat: shadcn admin启动

--- app/exchange/page.tsx
+++ app/exchange/page.tsx
@@ -29,11 +29,54 @@ export default function Page() {
             </div>
             <div className="line h-10 w-[1px] bg-[#2b2e33] ml-2 mr-4"></div>
           </div>
+          <div className="flex items-center gap-4">
+            <div className="coin-price">
+              <p className="text-[#f2f4f6] text-base font-bold">2.7024</p>
+              <p className="text-[#87909f] text-xs">RM 11.95</p>
+            </div>
+            <div className="24h-high">
+              <p className="text-[#87909f] text-xs">24h 最高价</p>
+              <p className="text-[#f2f4f6] text-xs mt-1">2.7495</p>
+            </div>
+            <div className="24h-low">
+              <p className="text-[#87909f] text-xs">24h 最低价</p>
+              <p className="text-[#f2f4f6] text-xs mt-1">2.6746</p>
+            </div>
+            <div className="24h-change">
+              <p className="text-[#87909f] text-xs">24h 成交量 (MX)</p>
+              <p className="text-[#f2f4f6] text-xs mt-1">2.8M</p>
+            </div>
+            <div className="24h-volume">
+              <p className="text-[#87909f] text-xs">24h 成交额 (USDT)</p>
+              <p className="text-[#f2f4f6] text-xs mt-1">7.83M</p>
+            </div>
+          </div>
           <div className="cursor-pointer absolute right-4">
             <PlayCircleOutlined />
           </div>
         </div>
-        <div className="wrapper-markets">自选币列表</div>
+        <div className="wrapper-markets px-4">
+          <div
+            className="wrapper-markets-title flex items-center gap-4 text-sm text-[#87909f] h-10"
+            style={{ borderBottom: "1px solid #222429" }}
+          >
+            <p>自选</p>
+            <p className="text-[#f2f4f6]">USDT</p>
+            <p>USDC</p>
+            <p>ETH</p>
+            <p>BTC</p>
+          </div>
+          <div
+            className="wrapper-markets-qu flex items-center justify-between gap-3 text-xs text-[#87909f] h-10"
+            style={{ borderBottom: "1px solid #222429" }}
+          >
+            <p className="text-[#f2f4f6]">全部</p>
+            <p>主板区</p>
+            <p>创新区</p>
+            <p>考核区</p>
+            <p>Trump</p>
+          </div>
+        </div>
         <div className="wrapper-kline">
           <div
             className="orderbook-header flex items-center gap-4 h-10 text-sm px-4"
