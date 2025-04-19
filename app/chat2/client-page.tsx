19173eff902b081c5ac071838568d880a2bd73a0
Fund <fundgao@gmail.com>
2025-04-19 18:59:26 +0800

    feat: shadcn admin启动

--- app/chat2/client-page.tsx
+++ app/chat2/client-page.tsx
@@ -31,7 +31,7 @@ export default function ClientPage({
           onClick={handleSubmit}
           className="px-4 py-2 bg-blue-500 text-white rounded"
         >
-          Send
+          发送
         </button>
       </div>
       <div className="space-y-4">
