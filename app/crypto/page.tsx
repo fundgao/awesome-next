6e1bdd77461e453537426cded2d815ffed3ad463
Fund <fundgao@gmail.com>
2025-04-22 14:55:34 +0800

    feat: shadcn admin启动

--- app/crypto/page.tsx
+++ app/crypto/page.tsx
@@ -8,6 +8,19 @@ import { Footer } from "@/components/ui/Footer";
 export default function Page() {
   return (
     <>
+      <title>
+        MEXC 交易所：您获得加密货币最简单的方式 -
+        交易比特币、以太坊和最热门的代币
+      </title>
+      <meta
+        name="description"
+        content="MEXC 是您获取加密货币的最简单方式。探索世界领先的加密货币交易所，购买、交易和赚取加密货币。交易比特币 BTC、以太坊 ETH 以及 3,000 多种代币。"
+      ></meta>
+      <link
+        rel="shortcut icon"
+        href="https://static.mocortech.com/image-host/web/favicon/favicon.ico"
+        type="image/x-icon"
+      ></link>
       <NavigationMenuMexc />
       <div className="relative w-full bg-[#0d0e0f]">
         <div className="content relative max-w-[1200px] mx-auto">
