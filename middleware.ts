78c95fc6788b761ccf7c0090001e992c83d2a784
Fund <fundgao@gmail.com>
2025-04-25 09:58:27 +0800

    feat: shadcn admin启动

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export default clerkMiddleware();
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+  // return NextResponse.redirect(new URL("/home", request.url));
 }
 
 export const config = {
