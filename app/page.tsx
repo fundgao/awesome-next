3d7d8776e72f583dff1c8c6b068a24836f7d56ab
Fund <fundgao@gmail.com>
2025-05-04 14:10:59 +0800

    fix: index icon size

--- app/page.tsx
+++ app/page.tsx
@@ -74,121 +74,123 @@ export default function Home() {
         </div>
         <div className="content-bottom absolute w-full bottom-[100px]">
           <div className="icons flex justify-center flex-wrap gap-x-8 gap-y-4">
-            <Link href="https://goldog.netlify.app/" target="_blank">
+            <Link href="https://goldog.netlify.app/" target="_blank" className="group">
               <Tilt rotationFactor={30} isRevese>
                 <div className={classIcon}>
                   <Image
                     src="/image/ai16z2.avif"
                     alt="ai16z Logo"
-                    className="rounded-3xl"
+                    className="rounded-3xl object-cover w-[80px] h-[80px]"
                     width={80}
                     height={80}
                     priority
                   />
                 </div>
               </Tilt>
-              <p className="text-center text-white mt-1 font-semibold">
+              <p className="text-center text-white mt-1 font-semibold group-hover:underline">
                 Goldoge
               </p>
             </Link>
-            <Link href="https://goldoge.vercel.app/" target="_blank">
+            <Link href="https://goldoge.vercel.app/" target="_blank" className="group">
               <Tilt rotationFactor={30} isRevese>
                 <div className={classIcon}>
                   <Image
                     src="/image/goldoge.jpeg"
                     alt="goldoge Logo"
-                    className="rounded-3xl"
+                    className="rounded-3xl object-cover w-[80px] h-[80px]"
                     width={80}
                     height={80}
                     priority
                   />
                 </div>
               </Tilt>
-              <p className="text-center text-white mt-1 font-semibold">
+              <p className="text-center text-white mt-1 font-semibold group-hover:underline">
                 金狗科技
               </p>
             </Link>
-            <Link href="https://ct.app" target="_blank">
+            <Link href="https://ct.app" target="_blank" className="group">
               <Tilt rotationFactor={30} isRevese>
                 <div className={classIcon}>
                   <Cointool />
                 </div>
               </Tilt>
-              <p className="text-center text-white mt-1 font-semibold">
+              <p className="text-center text-white mt-1 font-semibold group-hover:underline">
                 Cointool
               </p>
             </Link>
-            <Link href="https://docs.qq.com/desktop/" target="_blank">
+            <Link href="https://docs.qq.com/desktop/" target="_blank" className="group">
               <Tilt rotationFactor={30} isRevese>
                 <div className={classIcon}>
                   <Image
                     src="/image/wendang.png"
                     alt="腾讯文档 Logo"
-                    className="rounded-3xl"
+                    className="rounded-3xl object-cover w-[80px] h-[80px]"
                     width={80}
                     height={80}
                     priority
                   />
                 </div>
               </Tilt>
-              <p className="text-center text-white mt-1 font-semibold">
+              <p className="text-center text-white mt-1 font-semibold group-hover:underline">
                 腾讯文档
               </p>
             </Link>
             <Link
               href="https://www.mexc.com/zh-TW/support/categories/360000254192?utm_source=mexc&utm_medium=webmenu&utm_campaign=announcements"
               target="_blank"
+              className="group"
             >
               <Tilt rotationFactor={30} isRevese>
                 <div className={classIcon}>
                   <Image
                     src="/image/mexc.jpeg"
                     alt="mexc Logo"
-                    className="rounded-3xl"
+                    className="rounded-3xl object-cover w-[80px] h-[80px]"
                     width={80}
                     height={80}
                     priority
                   />
                 </div>
               </Tilt>
-              <p className="text-center text-white mt-1 font-semibold">
+              <p className="text-center text-white mt-1 font-semibold group-hover:underline">
                 抹茶公告
               </p>
             </Link>
             <Link
               href="https://www.cryptohunt.ai/zh-CN/dashboard/twitterTags"
               target="_blank"
+              className="group"
             >
               <Tilt rotationFactor={30} isRevese>
                 <div className={classIcon}>
                   <Image
                     src="/image/x.jpg"
                     alt="cryptohunt Logo"
-                    className="rounded-3xl"
+                    className="rounded-3xl object-cover w-[80px] h-[80px]"
                     width={80}
                     height={80}
                     priority
                   />
                 </div>
               </Tilt>
-              <p className="text-center text-white mt-1 font-semibold">
+              <p className="text-center text-white mt-1 font-semibold group-hover:underline">
                 热门话题
               </p>
             </Link>
-            <Link href="https://www.deepseek.com/" target="_blank">
+            <Link href="https://www.deepseek.com/" target="_blank" className="group">
               <Tilt rotationFactor={30} isRevese>
                 <div className={classIcon}>
                   <Image
                     src="/image/deepseek.ico"
                     alt="deepseek Logo"
-                    className="rounded-3xl"
+                    className="rounded-3xl object-cover w-[80px] h-[80px]"
                     width={80}
                     height={80}
                     priority
                   />
                 </div>
               </Tilt>
-              <p className="text-center text-white mt-1 font-semibold">
+              <p className="text-center text-white mt-1 font-semibold group-hover:underline">
                 deepseek
               </p>
             </Link>
@@ -196,6 +198,7 @@ export default function Home() {
             <Link
               href="https://coinmarketcap.com/zh/charts/fear-and-greed-index/"
               target="_blank"
+              className="group"
             >
               <Tilt rotationFactor={30} isRevese>
                 <div className={classIcon}>
@@ -209,7 +212,7 @@ export default function Home() {
                   />
                 </div>
               </Tilt>
-              <p className="text-center text-white mt-1 font-semibold">
+              <p className="text-center text-white mt-1 font-semibold group-hover:underline">
                 恐贪指数
               </p>
             </Link>
