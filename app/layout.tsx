faaa6fa6ce00659b08a0392afa71381a5e442e3f
Fund <fundgao@gmail.com>
2025-09-01 13:46:37 +0800

    fix: head title desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  // title: "FundOS - The Operating System for Fund",
-  // description: "FundOS is an open-source protocol for autonomous Fund.",
+  title: "FundOS - The Operating System for Fund",
+  description: "FundOS is an open-source protocol for autonomous Fund.",
   keywords: "Crypto | AIGC | Stock | X",
   openGraph: {
     type: "website",
