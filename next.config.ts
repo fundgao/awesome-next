3c9be238976ef84586750d8a5050ffa0c0d7777e
Fund <fundgao@gmail.com>
2025-12-09 11:09:06 +0800

    fix: React CVE-2025-55182漏洞修复

--- next.config.ts
+++ next.config.ts
@@ -12,14 +12,6 @@ const nextConfig: NextConfig = {
   experimental: {
     // DEMO https://github.com/vercel/next-view-transition-example/blob/main/app/card/page.tsx
     viewTransition: true,
-    useCache: true,
-    turbo: {
-      // Example: adding an alias and custom file extension
-      resolveAlias: {
-        underscore: "lodash",
-      },
-      resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".json"],
-    },
   },
 };
 
